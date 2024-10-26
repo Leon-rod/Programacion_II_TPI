df47a4194e393b77d436e31dec5ff596d8ba7457
412061-ramiroibazeta <tu_correo@example.com412061@tecnicatura.frc.utn.edu.ar>
2024-10-26 13:17:45 -0300

    Controlador, repositorio, servicio de Pedidos

---
+++ API/FarmaceuticaBack/FarmaceuticaBack/Data/Repositories/PedidoRepository.cs
@@ -0,0 +1,69 @@
+﻿using FarmaceuticaBack.Data.Contracts;
+using FarmaceuticaBack.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmaceuticaBack.Data.Repositories
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        private readonly FarmaceuticaContext _context;
+
+        public PedidoRepository(FarmaceuticaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Pedido>> GetAll()
+        {
+            var pedidos = await _context.Pedidos.ToListAsync();
+            return pedidos;
+        }
+
+        public async Task<List<Pedido>> GetByEstablecimiento(int id)
+        {
+            var pedidos = await _context.Pedidos                
+                .Include(c => c.IdPersonalCargosEstablecimientosNavigation)
+                .Where(e => e.IdPersonalCargosEstablecimientosNavigation.IdEstablecimiento == id)
+                .ToListAsync();
+            return pedidos;
+        }
+
+        public async Task<List<Pedido>> GetByFecha(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            DateOnly fromDateOnly = DateOnly.FromDateTime(fechaDesde);
+            DateOnly toDateOnly = DateOnly.FromDateTime(fechaHasta);
+
+            var pedidos = await _context.Pedidos
+                .Where(p => p.Fecha > fromDateOnly && p.Fecha < toDateOnly)
+                .ToListAsync();
+
+            return pedidos;
+        }
+
+        public async Task<Pedido> GetById(int id)
+        {
+            var p = await _context.Pedidos
+                .Include(p => p.DetallesPedidos)
+                .ThenInclude(p => p.IdMedicamentoLoteNavigation)
+                .Include(p => p.DetallesPedidos)
+                .ThenInclude(p => p.IdProductoNavigation)
+                .FirstOrDefaultAsync(p => p.IdPedido == id);
+            
+            return p;
+        }
+
+        public async Task<List<Pedido>> GetByLogistica(string cuit)
+        {
+            var pedidos = await _context.Pedidos
+                .Include(c => c.IdLogisticaNavigation)
+                .Where(e => e.IdLogisticaNavigation.Cuit == cuit)
+                .ToListAsync();
+            return pedidos;
+        }
+    }
+}
