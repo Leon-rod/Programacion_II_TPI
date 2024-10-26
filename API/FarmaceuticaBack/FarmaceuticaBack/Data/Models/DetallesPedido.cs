e7814d0aed6f3bc6273495d34117a66deb26221a
Franco Catania <francocatania93@gmail.com>
2024-10-26 15:38:07 -0300

    Se elimina json ignore de pedidos

--- API/FarmaceuticaBack/FarmaceuticaBack/Data/Models/DetallesPedido.cs
+++ API/FarmaceuticaBack/FarmaceuticaBack/Data/Models/DetallesPedido.cs
@@ -23,7 +23,7 @@ public partial class DetallesPedido
     public decimal PrecioUnitario { get; set; }
 
     public virtual MedicamentosLote IdMedicamentoLoteNavigation { get; set; }
-    [JsonIgnore]
+
     public virtual Pedido IdPedidoNavigation { get; set; }
 
     public virtual Producto IdProductoNavigation { get; set; }
